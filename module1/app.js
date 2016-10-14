62f7cc03af7ca30608ce942598885e560b9fdad7
Maria Cecilia C. Caluya(KPPH-RPb5n0117D) <PB5N0117@calcomp.com.tw>
2016-10-14 16:49:46 +0800

    Module1 coding assignment solution

---
+++ module1/app.js
@@ -0,0 +1,35 @@
+(function () {
+'use strict';
+
+angular.module('ModuleOneApp', [])
+.controller('ListController', ListController);
+
+ListController.$inject = ['$scope'];
+
+function ListController($scope) {
+	$scope.list = "";
+	$scope.listMessage = "Please enter data first.";
+
+	$scope.checkList = function () {
+		checkItems($scope.list);
+	};
+
+	function checkItems(list) {
+		var a_list = list.split(',');
+		var len = 0;
+		
+		for (var i = a_list.length - 1; i >= 0; i--) {
+			if (a_list[i].length > 0) {
+				len += 1;
+			}
+		}
+
+		if (len <= 3) {
+			$scope.listMessage = "Thanks!";
+		} else {
+			$scope.listMessage = "Too much!";
+		}
+	}
+}
+
+})();
